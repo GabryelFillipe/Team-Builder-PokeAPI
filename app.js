6f2b4196cae9f998b8eac2f85f492a9626a4af74
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@CFP127ALUNO.LOCAL>
2025-10-08 10:26:37 -0300

    Ajustando logica do JS

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ async function buscarTodosPokemons() {
         listaDeTodosPokemons = await Promise.all(promessasDeDetalhes);
         
         console.log('Todos os Pokémon foram carregados!');
+        console.log(listaDeTodosPokemons)
         return listaDeTodosPokemons
         
     } catch (error) {
@@ -31,176 +32,172 @@ async function buscarTodosPokemons() {
     return listaDeTodosPokemons
 }
 
-function limparElemento(elemento) {
-    while (elemento.firstChild) {
-        elemento.removeChild(elemento.firstChild);
+function criarOverlayDeCarregamento() {
+        const overlay = document.createElement('div');
+        overlay.className = 'loading-overlay';
+    
+        const spinner = document.createElement('div');
+        spinner.className = 'spinner';
+    
+        const texto = document.createElement('p');
+        texto.textContent = 'Carregando Pokédex...';
+    
+        overlay.appendChild(spinner);
+        overlay.appendChild(texto);
+    
+        document.body.appendChild(overlay);
+        return overlay;
     }
-}
+
+// function limparElemento(elemento) {
+//     while (elemento.firstChild) {
+//         elemento.removeChild(elemento.firstChild);
+//     }
+// }
 
 
-function abrirModalDeSelecao() {
+// function abrirModalDeSelecao() {
 
-    const modalExistente = document.getElementById('modal-pokemons')
-    if (modalExistente) modalExistente.remove()
+//     const modalExistente = document.getElementById('modal-pokemons')
+//     if (modalExistente) modalExistente.remove()
 
-    const overlay = document.createElement('div')
-    overlay.className = 'modal-overlay'
+//     const overlay = document.createElement('div')
+//     overlay.className = 'modal-overlay'
 
-    const modal = document.createElement('div')
-    modal.id = 'modal-pokemons'
-    modal.className = 'modal-content'
-
-
-    const modalHeader = document.createElement('div')
-    modalHeader.className = 'modal-header'
-    const titulo = document.createElement('h2')
-    titulo.textContent = 'Escolha um Pokémon'
-    const botaoFechar = document.createElement('button')
-    botaoFechar.className = 'close-button'
-    botaoFechar.textContent = '×'
-    modalHeader.appendChild(titulo)
-    modalHeader.appendChild(botaoFechar)
+//     const modal = document.createElement('div')
+//     modal.id = 'modal-pokemons'
+//     modal.className = 'modal-content'
 
 
-    const modalBody = document.createElement('div')
-    modalBody.className = 'modal-body'
-    const barraDeBusca = document.createElement('input')
-    barraDeBusca.type = 'text'
-    barraDeBusca.className = 'search-bar'
-    barraDeBusca.placeholder = 'Buscar Pokémon...'
-    const listaContainer = document.createElement('div')
-    listaContainer.className = 'pokemon-list'
-    modalBody.appendChild(barraDeBusca)
-    modalBody.appendChild(listaContainer)
+//     const modalHeader = document.createElement('div')
+//     modalHeader.className = 'modal-header'
+//     const titulo = document.createElement('h2')
+//     titulo.textContent = 'Escolha um Pokémon'
+//     const botaoFechar = document.createElement('button')
+//     botaoFechar.className = 'close-button'
+//     botaoFechar.textContent = '×'
+//     modalHeader.appendChild(titulo)
+//     modalHeader.appendChild(botaoFechar)
 
 
-    modal.appendChild(modalHeader)
-    modal.appendChild(modalBody)
-    document.body.appendChild(overlay)
-    document.body.appendChild(modal)
-
-    const fecharModal = () => {
-        modal.remove()
-        overlay.remove()
-    };
-
-    overlay.addEventListener('click', fecharModal)
-    botaoFechar.addEventListener('click', fecharModal)
+//     const modalBody = document.createElement('div')
+//     modalBody.className = 'modal-body'
+//     const barraDeBusca = document.createElement('input')
+//     barraDeBusca.type = 'text'
+//     barraDeBusca.className = 'search-bar'
+//     barraDeBusca.placeholder = 'Buscar Pokémon...'
+//     const listaContainer = document.createElement('div')
+//     listaContainer.className = 'pokemon-list'
+//     modalBody.appendChild(barraDeBusca)
+//     modalBody.appendChild(listaContainer)
 
 
-    popularListaPokemon()
-    ativarBuscaPokemon()
-}
+//     modal.appendChild(modalHeader)
+//     modal.appendChild(modalBody)
+//     document.body.appendChild(overlay)
+//     document.body.appendChild(modal)
+
+//     const fecharModal = () => {
+//         modal.remove()
+//         overlay.remove()
+//     };
+
+//     overlay.addEventListener('click', fecharModal)
+//     botaoFechar.addEventListener('click', fecharModal)
 
 
-function popularListaPokemon(lista = listaDeTodosPokemons) {
+//     popularListaPokemon()
+//     ativarBuscaPokemon()
+// }
+
+
+// function popularListaPokemon(lista = listaDeTodosPokemons) {
     
     
-    const containerLista = document.querySelector('.pokemon-list')
-    if (!containerLista) 
-        return limparElemento(containerLista)
+//     const containerLista = document.querySelector('.pokemon-list')
+//     if (!containerLista) 
+//         return limparElemento(containerLista)
 
-    if (lista.length === 0) {
-        const mensagemVazio = document.createElement('p');
-        mensagemVazio.textContent = 'Nenhum Pokémon encontrado.'
-        containerLista.appendChild(mensagemVazio);
-        return
-    }
-    const fragmento = document.createDocumentFragment()
+//     if (lista.length === 0) {
+//         const mensagemVazio = document.createElement('p');
+//         mensagemVazio.textContent = 'Nenhum Pokémon encontrado.'
+//         containerLista.appendChild(mensagemVazio);
+//         return
+//     }
+//     const fragmento = document.createDocumentFragment()
 
-    lista.forEach(pokemon => {
-        const item = document.createElement('div')
-        item.className = 'pokemon-list-item'
+//     lista.forEach(pokemon => {
+//         const item = document.createElement('div')
+//         item.className = 'pokemon-list-item'
 
         
-        const imagem = document.createElement('img')
-        imagem.src = pokemon.sprites.front_default || './img/pokeball-fundo.png'
-        imagem.alt = pokemon.name;
-        imagem.loading = 'lazy';
+//         const imagem = document.createElement('img')
+//         imagem.src = pokemon.sprites.front_default || './img/pokeball-fundo.png'
+//         imagem.alt = pokemon.name;
+//         imagem.loading = 'lazy';
 
-        const nome = document.createElement('span');
-        nome.textContent = pokemon.name;
+//         const nome = document.createElement('span');
+//         nome.textContent = pokemon.name;
 
-        item.appendChild(imagem);
-        item.appendChild(nome);
-        item.addEventListener('click', () => selecionarPokemon(pokemon))
-        fragmento.appendChild(item)
-    })
+//         item.appendChild(imagem);
+//         item.appendChild(nome);
+//         item.addEventListener('click', () => selecionarPokemon(pokemon))
+//         fragmento.appendChild(item)
+//     })
 
-    containerLista.appendChild(fragmento)
-}
-
-
-function ativarBuscaPokemon() {
-    const inputBusca = document.querySelector('.search-bar');
-    if (!inputBusca) return;
-
-    inputBusca.addEventListener('input', (evento) => {
-        const termoBuscado = evento.target.value.toLowerCase();
-        const pokemonsFiltradros = listaDeTodosPokemons.filter(pokemon => {
-            return pokemon.name.toLowerCase().includes(termoBuscado);
-        });
-        popularListaPokemon(pokemonsFiltradros);
-    });
-}
+//     containerLista.appendChild(fragmento)
+// }
 
 
-function selecionarPokemon(pokemon) {
-    if (cardClicado) {
-        const imgDoCard = cardClicado.querySelector('img');
-        const pDoCard = cardClicado.querySelector('p');
-        imgDoCard.src = pokemon.sprites.front_default;
-        pDoCard.textContent = pokemon.name;
+// function ativarBuscaPokemon() {
+//     const inputBusca = document.querySelector('.search-bar');
+//     if (!inputBusca) return;
+
+//     inputBusca.addEventListener('input', (evento) => {
+//         const termoBuscado = evento.target.value.toLowerCase();
+//         const pokemonsFiltradros = listaDeTodosPokemons.filter(pokemon => {
+//             return pokemon.name.toLowerCase().includes(termoBuscado);
+//         });
+//         popularListaPokemon(pokemonsFiltradros);
+//     });
+// }
 
 
-        cardClicado.dataset.pokemonId = pokemon.id;
-    }
-
-    const modal = document.getElementById('modal-pokemons');
-    const overlay = document.querySelector('.modal-overlay');
-    if (modal) modal.remove();
-    if (overlay) overlay.remove();
-}
+// function selecionarPokemon(pokemon) {
+//     if (cardClicado) {
+//         const imgDoCard = cardClicado.querySelector('img');
+//         const pDoCard = cardClicado.querySelector('p');
+//         imgDoCard.src = pokemon.sprites.front_default;
+//         pDoCard.textContent = pokemon.name;
 
 
+//         cardClicado.dataset.pokemonId = pokemon.id;
+//     }
 
-function criarOverlayDeCarregamento() {
-    const overlay = document.createElement('div');
-    overlay.className = 'loading-overlay';
+//     const modal = document.getElementById('modal-pokemons');
+//     const overlay = document.querySelector('.modal-overlay');
+//     if (modal) modal.remove();
+//     if (overlay) overlay.remove();
+// }
 
-    const spinner = document.createElement('div');
-    spinner.className = 'spinner';
-
-    const texto = document.createElement('p');
-    texto.textContent = 'Carregando Pokédex...';
-
-    overlay.appendChild(spinner);
-    overlay.appendChild(texto);
-
-    document.body.appendChild(overlay);
-    return overlay;
-}
+// function inicializarCards() {
+//     const cards = document.querySelectorAll('.card-pokemon');
+//     cards.forEach(card => {
+//         card.addEventListener('click', () => {
+//             cardClicado = card;
+//             if (listaDeTodosPokemons.length > 0) {
+//                 abrirModalDeSelecao();
+//             } else {
+//                 alert("Aguarde, os Pokémon ainda estão sendo carregados!");
+//             }
+//         })
+//     })
+// }
 
 
 
-
-function inicializarCards() {
-    const cards = document.querySelectorAll('.card-pokemon');
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            cardClicado = card;
-            if (listaDeTodosPokemons.length > 0) {
-                abrirModalDeSelecao();
-            } else {
-                alert("Aguarde, os Pokémon ainda estão sendo carregados!");
-            }
-        })
-    })
-}
-
-
-
-document.addEventListener('DOMContentLoaded', async () => {
-    await buscarTodosPokemons()
-    inicializarCards()
-})
+// // document.addEventListener('DOMContentLoaded', async () => {
+// //     await buscarTodosPokemons()
+// //     inicializarCards()
+// // })
+buscarTodosPokemons()
